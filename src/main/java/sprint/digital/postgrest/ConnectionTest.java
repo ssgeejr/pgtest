d95883fe9b583a0b817fbc949a0be70604a3ce40
ioexcept <ioexcept@gmail.com>
2019-11-18 10:58:43 -0600

    made a few changes so you can test from the CLI

--- src/main/java/sprint/digital/postgrest/ConnectionTest.java
+++ src/main/java/sprint/digital/postgrest/ConnectionTest.java
@@ -3,22 +3,26 @@ package sprint.digital.postgrest;
 import java.sql.*;
 
 public class ConnectionTest {
-
+	private boolean isLocal = false;
 	public ConnectionTest() {
 		
 	}
 	public ConnectionTest(boolean isLocalTest) {
 		//not used, but available for testing from the CLI
+		isLocal = isLocalTest;
 	}
 	
 	public String fetchResults() throws Exception{
 		Connection psqlConn = null;
 		StringBuffer result = new StringBuffer();
 		try {
-			psqlConn = DriverManager.getConnection("jdbc:postgresql://pgres:5432/postgres", "magicbox", "magicbox");
+			psqlConn = DriverManager.getConnection("jdbc:postgresql://18.224.58.28:5432/postgres", "magicbox", "magicbox");
 			ResultSet rset = psqlConn.createStatement().executeQuery("select loc_type_nme,lat_long_nbr,enb_id from loc limit 5");
 			while(rset.next()) {
-				result.append("<tr><td>" + rset.getString("loc_type_nme") + "</td><td>" + rset.getString("lat_long_nbr") + "</td><td>" + rset.getString("enb_id") + "</td></tr>");
+				if(isLocal)
+					System.out.println("loc_type_name [" + rset.getString("loc_type_nme") + "] lat_long_nbr [" + rset.getString("lat_long_nbr") + "] enb_id [" + rset.getString("enb_id") + "] ");
+				else
+					result.append("<tr><td>" + rset.getString("loc_type_nme") + "</td><td>" + rset.getString("lat_long_nbr") + "</td><td>" + rset.getString("enb_id") + "</td></tr>");
 			}
 			return result.toString();
 		}finally {
@@ -31,7 +35,11 @@ public class ConnectionTest {
 //	Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/example", "postgres", "postgres")
 
 	public static void main(String[] args) {
-		new ConnectionTest(true);
+		try {
+			new ConnectionTest(true).fetchResults();
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
